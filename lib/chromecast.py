7718a94c02a0d9d81fda78d410b8f37eb21d348b
Matthew Fong <jmattfong@gmail.com>
2019-09-29 01:18:54 -0700

    Loop blank video, wait correct times, and add cli

--- lib/chromecast.py
+++ lib/chromecast.py
@@ -9,28 +9,33 @@ import pprint
 
 DEVICE_NAME='Basement TV'
 
+BLANK_VIDEO=('GA_Buffer_Black_V.mp4', 'Blank Buffer', 10)
+BLANK_TIME=BLANK_VIDEO[2]
+
 VIDEOS=[
-'GA_Beauty_Roamer1_Win_V.mp4',
-'GA_Beauty_Roamer2_Win_V.mp4',
-'GA_Beauty_Roamer3_Win_V.mp4',
-'GA_Beauty_Roamer4_Win_V.mp4',
-'GA_Beauty_Startler_Win_V.mp4',
-'GA_Girl_Roamer1_Win_V.mp4',
-'GA_Girl_Roamer2_Win_V.mp4',
-'GA_Girl_Roamer3_Win_V.mp4',
-'GA_Girl_Roamer4_Win_V.mp4',
-'GA_Girl_Startler_Win_V.mp4',
-'GA_HeadOfHouse_Roamer1_Win_V.mp4',
-'GA_HeadOfHouse_Roamer2_Win_V.mp4',
-'GA_HeadOfHouse_Roamer3_Win_V.mp4',
-'GA_HeadOfHouse_Roamer4_Win_V.mp4',
-'GA_HeadOfHouse_Startler_Win_V.mp4',
-'GA_Wraith_Roamer1_Win_V.mp4',
-'GA_Wraith_Roamer2_Win_V.mp4',
-'GA_Wraith_Roamer3_Win_V.mp4',
-'GA_Wraith_Roamer4_Win_V.mp4',
-'GA_Wraith_Startler_Win_V.mp4'
+('GA_Beauty_Roamer1_Win_V.mp4', 'Beckoning Beauty 1', 32),
+('GA_Beauty_Roamer2_Win_V.mp4', 'Beckoning Beauty 2', 34),
+('GA_Beauty_Roamer3_Win_V.mp4', 'Beckoning Beauty 3', 45),
+('GA_Beauty_Roamer4_Win_V.mp4', 'Beckoning Beauty 4', 30),
+('GA_Beauty_Startler_Win_V.mp4', 'Beckoning Beauty Startler', 14),
+('GA_Girl_Roamer1_Win_V.mp4', 'Ghoulish Girl 1', 29),
+('GA_Girl_Roamer2_Win_V.mp4', 'Ghoulish Girl 2', 29),
+('GA_Girl_Roamer3_Win_V.mp4', 'Ghoulish Girl 3', 27),
+('GA_Girl_Roamer4_Win_V.mp4', 'Ghoulish Girl 4', 33),
+('GA_Girl_Startler_Win_V.mp4', 'Ghoulish Girl Startler', 12),
+('GA_HeadOfHouse_Roamer1_Win_V.mp4', 'Head of the House 1', 27),
+('GA_HeadOfHouse_Roamer2_Win_V.mp4', 'Head of the House 2', 27),
+('GA_HeadOfHouse_Roamer3_Win_V.mp4', 'Head of the House 3', 29),
+('GA_HeadOfHouse_Roamer4_Win_V.mp4', 'Head of the House 4', 27),
+('GA_HeadOfHouse_Startler_Win_V.mp4', 'Head of the House Startler', 12),
+('GA_Wraith_Roamer1_Win_V.mp4', 'Wrathful Wraith 1', 25),
+('GA_Wraith_Roamer2_Win_V.mp4', 'Wrathful Wraith 2', 24),
+('GA_Wraith_Roamer3_Win_V.mp4', 'Wrathful Wraith 3', 25),
+('GA_Wraith_Roamer4_Win_V.mp4', 'Wrathful Wraith 4', 23),
+('GA_Wraith_Startler_Win_V.mp4', 'Wrathful Wraith Startler', 12)
 ]
+
+ALL_VIDEOS = [BLANK_VIDEO] + VIDEOS
 
 class ChromecastPlayer(object):
 
@@ -40,9 +45,13 @@ class ChromecastPlayer(object):
 
         self.server = server
         self.cast = ChromecastPlayer.chromecastConnect(deviceName)
+        self.mc = self.cast.media_controller
+        self.last_played = ''
+        self.next_video = None
+
         super().__init__()
 
-    def chromecastConnect(deviceName=DEVICE_NAME) :
+    def chromecastConnect(deviceName) :
         chromecasts = pychromecast.get_chromecasts()
         #print(chromecasts)
         cast = next(cc for cc in chromecasts if cc.device.friendly_name == deviceName)
@@ -50,26 +59,51 @@ class ChromecastPlayer(object):
         # Start worker thread and wait for cast device to be ready
         cast.wait()
 
-        print("Connected to chromecast: " + str(cast.device))
         print()
-        print("Chromecast Status: " + str(cast.status))
+        print("Connected to chromecast: " + str(cast.device))
         print()
 
         return cast
 
-    def playRandomVideo(self):
-        video = random.choice(VIDEOS)
-        self.playVideo(video)
+    def play_loop(self) :
+        while(True) :
+            sleep_time = BLANK_TIME
+            if self.next_video is None :
+                self.play_blank()
+            else :
+                sleep_time = self.next_video[2]
+                self.play_video(self.next_video)
+                print('Waiting ' + str(sleep_time) + ' seconds for video to finish')
+            self.next_video = None
+            time.sleep(sleep_time + 1)
 
-    def playVideo(self, videoFile):
+    def queue_random_video(self):
+        self.queue_video(random.choice(VIDEOS))
+
+    def queue_video(self, video):
+        self.next_video = video
+        print('Queued video ' + video[1])
+
+    def play_blank(self) :
+        if self.last_played != BLANK_VIDEO[0] :
+            print()
+            print('Playing blank video')
+            print()
+        self.play_video(BLANK_VIDEO, logs=False)
+
+    def play_video(self, video, logs=True):
+        (videoFile, videoName, videoLength) = video
         videoUrl = (self.server + '/' + videoFile)
-        print("Playing video " + str(videoUrl))
-        print()
 
-        mc = self.cast.media_controller
-        mc.play_media(videoUrl, content_type='video/mp4')
-        mc.block_until_active()
-        mc.play()
+        if logs :
+            print()
+            print("Playing video '" + videoName + "' with url " + str(videoUrl))
 
-        print("Chromecast media status :  " + str(mc.status))
-        print()
+        self.mc.play_media(videoUrl, content_type='video/mp4')
+        self.mc.block_until_active()
+        self.mc.play()
+        self.last_played = videoFile
+
+        if logs :
+            print("Media State :  " + str(self.mc.status.player_state))
+            print()
--- main.py
+++ main.py
@@ -1,38 +1,76 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import json
-import time
 import argparse
-from lib.chromecast import ChromecastPlayer
+import lib.chromecast as chromecast
 from lib.ring import RingEnhancedSpookinator
-
-BLACK_VIDEO='GA_Buffer_Black_V.mp4'
+import threading
 
 def main():
-    parser = argparse.ArgumentParser(description='Let\'s get spooky with some halloween good times.')
-    parser.add_argument('--run', choices=['chromecast', 'ring', 'both'], default='both', 
-                        help='pick if you want to run the chromecast portion or ring. Useful for testing.')
-    parser.add_argument('--server-url', help='the server url where the videos are being served')
-    parser.add_argument('--ring-config', help='the path to the config where that username and password is',
+    parser = argparse.ArgumentParser(
+        description='Let\'s get spooky with some halloween good times.')
+    parser.add_argument('--run',
+        choices=['chromecast', 'ring', 'both'],
+        help='pick if you want to run the chromecast portion or ring. Useful for testing.',
+        default='both')
+    parser.add_argument('--server-url',
+        help='the server url where the videos are being served',
+        default='https://jmattfong-halloween.s3.us-west-2.amazonaws.com')
+    parser.add_argument('--ring-config',
+        help='the path to the config where that username and password is',
         default='/var/secret/ring-cred.json')
 
     args = parser.parse_args()
 
-    # default to using the printAlert method when we are not combining this with actually showing a chromecast video
-    callback = printAlert
+    # default to using the print_alert method when we are not combining this with actually showing a chromecast video
+    random_video_callback = print_alert
+    play_video_callback = print_video
 
     if args.run == 'chromecast' or args.run == 'both':
-        chromeCaster = ChromecastPlayer(args.server_url)
-        callback = chromeCaster.playRandomVideo
-        chromeCaster.playVideo(BLACK_VIDEO)
+        print('Starting chromecast thread')
+        chrome_caster = chromecast.ChromecastPlayer(args.server_url)
+        random_video_callback = chrome_caster.queue_random_video
+        play_video_callback = chrome_caster.queue_video
+
+        chromecastThread = threading.Thread(target=runChromecast, args=(chrome_caster,))
+        chromecastThread.start()
 
     if args.run == 'ring' or args.run == 'both':
-       ring = RingEnhancedSpookinator(args.ring_config) 
-       ring.pollForActivity(callback)
+        print('Starting ring thread')
+        ringThread = threading.Thread(target=runRing, args=(args.ring_config, random_video_callback,))
+        ringThread.start()
 
-def printAlert():
-    print('Alert detected!')
+    print('Starting input thread')
+    inputThread = threading.Thread(target=runCli, args=(play_video_callback,))
+    inputThread.start()
+
+def runChromecast(chrome_caster) :
+    chrome_caster.play_loop()
+
+def runRing(ring_config, random_video_callback) :
+    ring = RingEnhancedSpookinator(ring_config)
+    ring.pollForActivity(random_video_callback)
+
+def runCli(play_video_callback) :
+    while(True) :
+        print()
+        print('Select a video to play')
+        for index, video in enumerate([video[1] for video in chromecast.ALL_VIDEOS], start=0):
+            print(str(index) + ('  - ' if index < 10 else ' - ') + str(video))
+        try :
+            inputNum = int(input('> '))
+            selectedVideo = chromecast.ALL_VIDEOS[inputNum]
+            play_video_callback(selectedVideo)
+        except:
+            print()
+            print("That's not a valid number :(")
+
+def print_video(video) :
+    print('No chromecast, not playing video: ' + str(video))
+
+def print_alert():
+    print('Alert detected! No chromecast')
 
 if __name__== "__main__" :
     main()
