30652b8c8f8efcd0668b1afb6064dec96cbe1c60
Matthew Fong <jmattfong@gmail.com>
2019-09-28 18:13:31 -0700

    Implement chromecast connect and play video methods

--- lib/chromecast.py
+++ lib/chromecast.py
@@ -7,16 +7,36 @@ import pychromecast
 
 DEVICE_NAME='Basement TV'
 
-def testChromecast():
+def main():
+    videoFile = sys.argv[1]
+    cast = chromecastConnect()
+    playVideo(cast, videoFile)
+
+def chromecastConnect(deviceName=DEVICE_NAME) :
     chromecasts = pychromecast.get_chromecasts()
-    cast = next(cc for cc in chromecasts if cc.device.friendly_name == DEVICE_NAME)
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == deviceName)
 
     # Start worker thread and wait for cast device to be ready
     cast.wait()
-    print(cast.device)
-    print(cast.status)
+
+    print("Connected to chromecast: " + str(cast.device))
+    print()
+    print("Chromecast Status: " + str(cast.status))
+    print()
+
+    return cast
+
+def playVideo(cast, videoFile):
+    print("Playing video " + str(videoFile))
+    print()
 
     mc = cast.media_controller
-    mc.play_media('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'video/mp4')
+    mc.play_media(videoFile, content_type='video/mp4')
     mc.block_until_active()
-    print(mc.status)
+    mc.play()
+
+    print("Chromecast media status :  " + str(mc.status))
+    print()
+
+if __name__== "__main__" :
+    main()
