0672933469b15cd4f5d8bb7f0486e53695d70da0
Matthew Fong <jmattfong@gmail.com>
2019-10-19 00:31:31 -0700

    Tried to write chromecast class in Typescript. Doesn't work. Help

--- lib/chromecast.ts
+++ lib/chromecast.ts
@@ -5,7 +5,8 @@ let DEVICE_NAME = 'Basement TV'
 let BLANK_VIDEO = ['GA_Buffer_Black_V.mp4', 'Blank Buffer', 10]
 let BLANK_TIME = BLANK_VIDEO[2]
 
-let VIDEOS = [['GA_Beauty_Roamer1_Win_V.mp4', 'Beckoning Beauty 1', 32],
+let VIDEOS = [
+['GA_Beauty_Roamer1_Win_V.mp4', 'Beckoning Beauty 1', 32],
 ['GA_Beauty_Roamer2_Win_V.mp4', 'Beckoning Beauty 2', 34],
 ['GA_Beauty_Roamer3_Win_V.mp4', 'Beckoning Beauty 3', 45],
 ['GA_Beauty_Roamer4_Win_V.mp4', 'Beckoning Beauty 4', 30],
@@ -36,38 +37,78 @@ let ALL_VIDEOS = VIDEOS.concat(BLANK_VIDEO)
 
 export class ChromecastPlayer {
 
-    private browser
-    private server: string
-    private current_video_start_time = 0
-    private current_video_duration = 0
+    private playingVideo: boolean = false
 
-    constructor(server: string, deviceName: string = DEVICE_NAME) {
+    private server: string
+
+    private device
+
+    constructor(server: string = 'https://jmattfong-halloween.s3.us-west-2.amazonaws.com', deviceName: string = DEVICE_NAME) {
         if (server == null || server === '') {
-            throw new Error('config path must set');
+            throw new Error('server url is null or empty');
         }
         this.server = server
-        this.browser = new ChromecastAPI.Browser()
 
-        this.startPlayingBlankVideo()
+        this.startPlayLoop()
     }
 
-    private startPlayingBlankVideo(): void {
-        // TODO
+    private startPlayLoop(): void {
+        var browser = new ChromecastAPI.Browser()
+        browser.on('deviceOn', function(device) {
+            this.device = device
+            this.playBlank()
+        })
+        return null
     }
 
     public playRandomVideo(): void {
+        this.playingVideo = true
         var video = VIDEOS[Math.floor(Math.random() * VIDEOS.length)];
         this.playVideo(video)
     }
 
-    public playVideo(video): void {
+    private playBlank(): void {
+        if (!this.playingVideo) {
+            this.playVideo(BLANK_VIDEO, false)
+        }
+    }
 
+    private playVideo(video, logs: boolean = true): void {
+        var url = video[0]
+        var name = video[1]
+        var lengthSeconds = video[2]
+
+        if (this.playingVideo) {
+            console.log('Another video is already playing, not playing video ' + name)
+        } else {
+            if (logs) {
+                console.log('Requesting video ' + name + ' with url ' + url)
+            }
+
+            this.device.play(url, 0, function() {
+                if (logs) {
+                    console.log('Playing video ' + name + ' with url ' + url)
+                }
+            })
+
+            setTimeout(function () {
+                console.log('Video ' + name + ' is over, going back to playing nothing')
+                this.playingVideo = false
+                this.playBlank()
+            }, lengthSeconds * 1000)
+        }
     }
 }
 
 /*
+// TODO remove
 async function main() {
-    const spook = new ChromecastPlayer("")
+    const chromecastPlayer = new ChromecastPlayer()
+
+    // Wait 20 seconds to start playing a video
+    setTimeout(function () {
+        chromecastPlayer.playRandomVideo()
+    }, 20000)
 }
 
 main()
