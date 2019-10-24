ca7ed5f64b07d592b97faf99e486b54ddc2055c3
Matthew Fong <jmattfong@gmail.com>
2019-10-24 00:31:39 -0700

    Add colour class

---
+++ lib/colour.ts
@@ -0,0 +1,17 @@
+export class Colour {
+    private x: number
+    private y: number
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX() {
+        return this.x;
+    }
+
+    public getY() {
+        return this.y;
+    }
+}
