012e5f726ac2526d5ca6ac10639bf16ae50adaf6
Matthew Fong <jmattfong@gmail.com>
2019-10-23 23:20:37 -0700

    refator spooky hue real time

--- lib/hue.ts
+++ lib/hue.ts
@@ -1,38 +1,41 @@
-import { readFileSync } from 'fs';
-
 const v3 = require('node-hue-api').v3;
 const LightState = v3.lightStates.LightState;
 
-const secretContents = readFileSync('./secrets/secrets.json', { encoding: 'utf-8' })
-let secrets = JSON.parse(secretContents);
-
-const USERNAME = secrets.hueUsername
-    // The name of the light we wish to retrieve by name
-    , LIGHT_ID = 3
-    ;
-
 export class SpookyHue {
+    private lightApi: any
+    private username: string
     constructor(username: string) {
-        v3.discovery.nupnpSearch()
-        .then(searchResults => {
-            console.log(`found ${searchResults.length} hubs. Connecting to the first one: ${searchResults[0]}`);
-            const host = searchResults[0].ipaddress;
-            console.log(`connecting to ${host}`)
-            v3.api.createLocal(host).connect(username)
+        this.username = username;
+    }
+
+    public async connect() {
+        let searchResults = await v3.discovery.nupnpSearch();
+        console.log(`found ${searchResults.length} hubs. Connecting to the first one: ${searchResults[0]}`);
+        const host = searchResults[0].ipaddress;
+        console.log(`connecting to ${host}`)
+        await v3.api.createLocal(host).connect(this.username);
+        console.log('connected!')
+    }
+
+    public async getLights() {
+        throw new Error('not implemented yet');
+    }
+
+    public async playPattern(lightId: number, patterns: LightPattern[]) {
+        console.log('playing light pattern');
+        patterns.forEach(async p => {
+            console.log(`playing pattern: ${typeof p}`);
+            await p.run(this.lightApi, lightId);
+            console.log('done playing pattern');
         });
-    }
 
-    public async start() {
-
-    }
-
-    public async assignCallbackToLights() {
-
+        console.log('dont playing all patterns. Setting light back to default state')
+        // set light back to default state
     }
 }
 
 export interface LightPattern {
-    run: (lightApi: any, lightObject: any) => void
+    run: (lightId: number, lightApi: any) => Promise<void>
 }
 
 export class FlickerPattern implements LightPattern {
@@ -41,22 +44,23 @@ export class FlickerPattern implements LightPattern {
         this.durationMs = durationSeconds * 1000;
     }
 
-    public async run(lightApi: any, lightObject: any): Promise<void> {
-        const start = new Date();
+    public async run(lightId: number, lightApi: any): Promise<void> {
+        const startTime = new Date();
 
         while (true) {
-
             const state = new LightState()
                 .on()
                 .ct(200)
-                .on(10 * 100)
                 .brightness(getRandomInt(100))
                 .transitiontime(0);
 
-            lightApi.lights.setLightState(lightObject.id, state);
+            lightApi.lights.setLightState(lightId, state);
+            const currTime = new Date();
+            if (currTime.getTime() - startTime.getTime() < this.durationMs) {
+                return;
+            }
+
             await sleep(getRandomInt(200) + 50);
-
-
         }
     }
 }
@@ -67,9 +71,9 @@ export class OffPattern implements LightPattern {
         this.durationMs = durationMs;
     }
 
-    public async run(lightApi: any, lightObject: any): Promise<void> {
+    public async run(lightId: number, lightApi: any): Promise<void> {
         const state = new LightState().off();
-        lightApi.lights.setLightState(lightObject.id, state);
+        lightApi.lights.setLightState(lightId, state);
         await sleep(getRandomInt(200) + 50);
     }
 }
@@ -88,14 +92,14 @@ export class StableColourPattern implements LightPattern {
         this.durationMs = durationMs;
     }
 
-    public async run(lightApi: any, lightObject: any): Promise<void> {
+    public async run(lightId: number, lightApi: any): Promise<void> {
         const state = new LightState()
             .on()
             .ct(200)
             .rgb(this.r, this.g, this.b)
             .brightness(this.brightness)
             .transitiontime(0);
-        lightApi.lights.setLightState(lightObject.id, state);
+        lightApi.lights.setLightState(lightId, state);
         await sleep(this.durationMs);
     }
 }
@@ -106,35 +110,4 @@ async function sleep(ms: number): Promise<void> {
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-}
-
-async function doMoreShit(api) {
-    var bright = 0;
-    while (true) {
-        console.log('Setting state to ' + (bright * 100))
-        // Using a LightState object to build the desired state
-        const state = new LightState()
-            .on()
-            .ct(200)
-            .on(bright * 100)
-            .brightness(getRandomInt(100))
-            .transitiontime(0)
-            ;
-        let attributesAndState = await api.lights.getLightAttributesAndState(LIGHT_ID);
-
-        console.log(attributesAndState["state"]["on"])
-        if (attributesAndState["state"]["on"] == false) {
-            // await sleep(300)
-        }
-
-        // .then(attributesAndState => {
-        //   // Display the details of the light
-        //   // console.log(JSON.stringify(attributesAndState, null, 2));
-
-        // });
-        bright = (bright + 1) % 2
-        api.lights.setLightState(LIGHT_ID, state);
-        api.lights.setLightState(2, state);
-        await sleep(getRandomInt(200) + 50)
-    }
 }
