45644df01c3c6009bfa87b82449967c10c5eae9d
Rory Jacob <rory@vendia.net>
2021-10-17 22:44:14 -0700

    Add setLogLevel function to logger

--- lib/logging.ts
+++ lib/logging.ts
@@ -4,9 +4,12 @@ import {Category,CategoryLogger,CategoryServiceFactory,CategoryConfiguration,Log
 
 // Optionally change default settings, in this example set default logging to Info.
 // Without changing configuration, categories will log to Error.
-CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info));
 
 export function getLogger(name: string): CategoryLogger {
     // Create categories, they will autoregister themselves, one category without parent (root) and a child category.
     return new Category(name);
+}
+
+export function setLogLevel(level: LogLevel) {
+    CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(level));
 }
