1ff7b78465050a880c17c5a302636a7b7638717a
Matthew Fong <johnfon@amazon.com>
2022-10-29 00:49:15 -0700

    Merge

--- src/lib/scene/scenes.ts
+++ src/lib/scene/scenes.ts
@@ -193,6 +193,103 @@ export class MultiPartScene extends Scene {
   }
 }
 
+export class SplitPartScene extends Scene {
+  ringSensorName: string;
+  hueSensorId?: number;
+  spookyHueEvents: Event[];
+  spookyRingEvents: Event[];
+  unSpookyEvents: Event[];
+  spookOnFaulted: boolean;
+
+  constructor(
+    ringSensorName: string,
+    spookyHueEvents: Event[],
+    spookyRingEvents: Event[],
+    unSpookyEvents: Event[],
+    hueSensorId?: number,
+    spookOnFaulted: boolean = false
+  ) {
+    super();
+    this.ringSensorName = ringSensorName;
+    this.spookyHueEvents = spookyHueEvents;
+    this.spookyRingEvents = spookyRingEvents;
+    this.unSpookyEvents = unSpookyEvents;
+    this.hueSensorId = hueSensorId;
+    this.spookOnFaulted = spookOnFaulted;
+  }
+
+  async setup(
+    _ringFunction: () => Promise<RingEnhancedSpookinatorV2>,
+    hueFunction: () => Promise<SpookyHueApi>
+  ): Promise<void> {
+    const spookyHueBulbPlayer = new SpookyHueBulbPlayer(await hueFunction());
+
+    this.ringCallback = [
+      this.ringSensorName,
+      (data: RingDeviceData) => {
+        log.info(`callback called on ${data.name}`);
+
+        // if the data.faulted is true, that means that the door is open
+        if (data.faulted) {
+          if (this.spookOnFaulted) {
+            this.spookyHueEvents.forEach((event) => {
+              event.cancel();
+            })
+            this.spookyRingEvents.forEach((event) => {
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          } else {
+            this.unSpookyEvents.forEach((event) => {
+              this.spookyRingEvents.forEach((event) => {
+                event.cancel();
+              })
+
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          }
+        } else {
+          if (this.spookOnFaulted) {
+            this.unSpookyEvents.forEach((event) => {
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          } else {
+            this.spookyHueEvents.forEach((event) => {
+              event.cancel();
+            });
+            this.spookyRingEvents.forEach((event) => {
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          }
+        }
+      },
+    ];
+
+    if (this.hueSensorId) {
+      this.hueCallback = [
+        this.hueSensorId,
+        (data: HueSensorUpdate) => {
+          log.info(
+            `callback called on ${this.hueSensorId} => ${data.getPresence()}`
+          );
+
+          if (data.getPresence()) {
+            this.spookyRingEvents.forEach((event) => {
+              event.cancel();
+            });
+            this.spookyHueEvents.forEach((event) => {
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          } else {
+            this.unSpookyEvents.forEach((event) => {
+              spookyHueBulbPlayer.playPattern(event);
+            });
+          }
+        },
+      ];
+    }
+  }
+}
+
 class RandomMultiScene extends Scene {
   ringSensorName: string;
   spookyEventChoices: Event[][];
