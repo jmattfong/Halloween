608b819c0cc628789870d6c23c7381f27072808e
Rory Jacob <rory@vendia.net>
2022-10-28 21:35:23 -0700

    Fix build issues with missing files

---
+++ src/lib/hue/light.ts
@@ -0,0 +1,22 @@
+import LightState = require("node-hue-api/lib/model/lightstate/LightState");
+import { Color } from "../config"
+
+export function createLightState(color: Color, transitionSeconds: number, brightness?: number): LightState {
+    if (brightness == null) {
+        brightness = color.bri
+    }
+    if (brightness > 254) {
+        brightness = 254
+    } else if (brightness < 1) {
+        brightness = 1
+    }
+    return new LightState()
+        .on(true)
+        .xy(color.xy[0], color.xy[1])
+        .hue(color.hue)
+        .sat(color.sat)
+        //.ct(color.ct)
+        .bri(brightness)
+        // Weird, but this is in increments of 100ms
+        .transitiontime(transitionSeconds * 10);
+}
