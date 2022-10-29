ac27c4f17fb37a5176c0145f6c7b036951b1e229
Edd Tsao <edd.tsao@carina.org>
2022-10-28 19:13:04 -0700

    Remove old implementations

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { SpookyHueApi } from "./lib/hue/hue";
 import { parse } from "ts-command-line-args";
 import { getLogger, setLogLevel } from "./lib/logging";
 import { CategoryLogger, LogLevel } from "typescript-logging";
-import { SCENES_2022 } from "./lib/scene/scenes_2022";
 import RingSensor from "./lib/triggers/sensors/RingSensor";
 import { scenes2022 } from "./collections/2022";
 import { InputTrigger } from "./lib/triggers/InputTrigger";
@@ -14,93 +13,6 @@ import { HueSensor } from "./lib/triggers/sensors/HueSensor";
 import { RemoteTrigger } from "./lib/triggers/RemoteTrigger";
 
 const log: CategoryLogger = getLogger("main");
-const SCENES = SCENES_2022;
-
-// For details about adding new args, see https://www.npmjs.com/package/ts-command-line-args
-interface IHalloweenServerArgs {
-  scene: string[];
-  webserverPort: number;
-  debug: boolean;
-  help?: boolean;
-}
-
-async function main() {
-  const { env } = process;
-
-  const args = parse<IHalloweenServerArgs>(
-    {
-      scene: {
-        type: String,
-        alias: "s",
-        multiple: true,
-        optional: true,
-        description: `The scene to run. Choose from: ${Object.keys(SCENES)}`,
-      },
-      webserverPort: {
-        type: Number,
-        defaultValue: 4343,
-        alias: "p",
-        optional: true,
-        description: `The port to run the webserver on. Defaults to (4343)`,
-      },
-      debug: {
-        type: Boolean,
-        alias: "d",
-        optional: true,
-        description: "Turn on debug logging",
-      },
-      help: {
-        type: Boolean,
-        optional: true,
-        alias: "h",
-        description: "Prints this usage guide",
-      },
-    },
-    {
-      helpArg: "help",
-      headerContentSections: [
-        {
-          header: "Halloween Spooktacular",
-          content: "Get ready to spook and be spooked",
-        },
-      ],
-    }
-  );
-
-  const logLevel = args.debug ? LogLevel.Debug : LogLevel.Info;
-  setLogLevel(logLevel);
-
-  log.info(`input args: ${JSON.stringify(args)}\n`);
-
-  var ringSpook: RingEnhancedSpookinatorV2;
-  const getRing = async () => {
-    if (ringSpook == null) {
-      log.info("Setting up Ring");
-      ringSpook = new RingEnhancedSpookinatorV2(CONFIG.secretPath, true);
-      log.debug(`all my sensors: ${await ringSpook.getSensors()}`);
-    }
-    return ringSpook;
-  };
-
-  var spookHue: SpookyHueApi;
-  const getHue = async () => {
-    if (spookHue == null) {
-      log.info("Setting up Hue");
-      spookHue = new SpookyHueApi(CONFIG.secretPath, CONFIG);
-      await spookHue.connectUsingIP(CONFIG.hue_bridge_ip);
-      log.debug(
-        `get all lights: ${(await spookHue.getLights()).map((l: any) =>
-          l.toStringDetailed()
-        )}`
-      );
-    }
-    return spookHue;
-  };
-
-  args.scene.forEach((s) => {
-    SCENES[s].start(getRing, getHue);
-  });
-}
 
 // For details about adding new args, see https://www.npmjs.com/package/ts-command-line-args
 interface IAltHalloweenServerArgs {
@@ -115,7 +27,7 @@ const hueDevices: HueSensor[] = [];
 export const hueApi = new SpookyHueApi(CONFIG.secretPath, CONFIG);
 export const ringApi = new RingEnhancedSpookinatorV2(CONFIG.secretPath, true);
 
-async function altMain() {
+async function main() {
   const args = parse<IAltHalloweenServerArgs>(
     {
       scene: {
@@ -123,7 +35,9 @@ async function altMain() {
         optional: true,
         alias: "s",
         multiple: true,
-        description: `The scene to run. Choose from: ${Object.keys(SCENES)}`,
+        description: `The scene to run. Choose from: ${Object.keys(
+          scenes2022
+        )}`,
       },
       debug: {
         type: Boolean,
@@ -171,4 +85,4 @@ async function altMain() {
   util_scenes;
 }
 
-altMain();
+main();
