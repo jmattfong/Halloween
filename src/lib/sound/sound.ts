519979a3e7179185583bc5c315e9d0a6e99c276f
caseyyh <cyx.huang@gmail.com>
2022-10-29 17:48:57 -0700

    SOund on windows# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch no-chromecast# Your branch is up to date with 'origin/no-chromecast'.## Changes to be committed:#	modified:   src/lib/sound/sound.ts#

--- src/lib/sound/sound.ts
+++ src/lib/sound/sound.ts
@@ -4,6 +4,20 @@ import { getLogger } from "../logging";
 import { CategoryLogger } from "typescript-logging";
 const log: CategoryLogger = getLogger("sound-player");
 
+const macPlayCommand = (path, volume) => `afplay \"${path}\" -v ${volume}`
+
+/* WINDOW PLAY COMMANDS */
+const addPresentationCore = `Add-Type -AssemblyName presentationCore;`
+const createMediaPlayer = `$player = New-Object system.windows.media.mediaplayer;`
+const loadAudioFile = path => `$player.open('${path}');`
+const playAudio = `$player.Play();`
+const stopAudio = `Start-Sleep 1; Start-Sleep -s $player.NaturalDuration.TimeSpan.TotalSeconds;Exit;`
+
+const windowPlayCommand = (path, volume) =>
+  `powershell -c ${addPresentationCore} ${createMediaPlayer} ${loadAudioFile(
+    path,
+  )} $player.Volume = ${volume}; ${playAudio} ${stopAudio}`
+
 export class SoundPlayer {
     private controller: AbortController;
     constructor() {
@@ -12,9 +26,16 @@ export class SoundPlayer {
     public async play(soundFile: string, volume: number) {
         this.controller = new AbortController();
         log.info(`attempting to play ${soundFile}`);
+
+	const volumeAdjustedByOs = process.platform == "darwin" ? Math.min(2, volume * 2) : volume;
+
+	const playCommand = process.platform == "darwin" ? macPlayCommand(soundFile, volumeAdjustedByOs) : windowPlayCommand(soundFile, volumeAdjustedByOs);
+
+	log.debug(`running command: ${playCommand}`);
+
         const { signal } = this.controller;
-        exec(`afplay ${soundFile} -v ${volume}`, { signal }, (_error) => {
-            log.info(`Canceled ${soundFile} playback`);
+        exec(playCommand, { signal }, (error) => {
+            log.info(`Sound ${soundFile} has been stopped. Error: ${error}`);
         });
     }
 
