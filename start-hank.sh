52a8e4ca121f8af25a9009fad38f6825f2c169b0
Matt <jmattfong@gmail.com>
2023-10-23 20:42:12 -0700

    Fix script output

--- start-hank.sh
+++ start-hank.sh
@@ -4,6 +4,6 @@ ORCHESTRATOR_IP=${1:-"192.168.86.24"}
 ORCHESTRATOR_PORT=${2:-8008}
 PORT=${3:-4242}
 
-echo "Starting client server BILL on port $PORT with orchestrator http://${ORCHESTRATOR_IP}:${ORCHESTRATOR_PORT}"
+echo "Starting client server HANK on port $PORT with orchestrator http://${ORCHESTRATOR_IP}:${ORCHESTRATOR_PORT}"
 
 ./run-client -o $ORCHESTRATOR_IP -x $ORCHESTRATOR_PORT -p $PORT -s down_bath_random
