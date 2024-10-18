480055093e264a917bd39a04623f29409cd5025c
Rory Jacob <roryjacob@gmail.com>
2024-10-18 12:52:14 -0700

    Add comments

--- src/lib/scene/scene_config.ts
+++ src/lib/scene/scene_config.ts
@@ -6,9 +6,19 @@ export class SceneConfig {
 }
 
 export class SceneDetails {
+  // the name of the scene
   name: string;
+
+  // the id of the sensor (ex: Waffles' Room)
   sensorId: string;
+
+  // the identifier of the sensor (ex: ring, hue)
   sensorType: string;
+
+  // whether to trigger the scene on fault or not
+  // for the ring sensor, faulted is door open and notFaulted is door closed
+  // for the hue sensor, faulted is sensed movement
+  // if set to null, the scene is triggered on either state
   onFault?: boolean;
 }
 
